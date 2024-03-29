b253ce7731f3dd5eed57350fc736ac2e887bf6ea
Francisca <frcastrog@uc.cl>
2024-03-29 11:45:40 +0100

    Initial commit

---
+++ 02-scripts/01-replication-script.R
@@ -0,0 +1,3 @@
+#-----------------------------Replication Script-------------------------------#
+#-Author: Francisca Castro ----------------------- Created: September 18, 2023-#
+#-R Version: 4.3.1 ----------------------------------- Revised: March 26, 2024-#
